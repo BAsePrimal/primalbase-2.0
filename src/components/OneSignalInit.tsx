b756cc8858e8b43b446179d49a1efd8e9aa7c743
Joao Vitor <joaovitorcoura@gmail.com>
2026-03-06 15:28:59 -0300

    modal alerta

--- src/components/OneSignalInit.tsx
+++ src/components/OneSignalInit.tsx
@@ -14,28 +14,7 @@ export default function OneSignalInit() {
           appId: process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID,
           safari_web_id: "web.onesignal.auto.64337d6b-67cd-4296-b41c-d41bc6b4a874",
           allowLocalhostAsSecureOrigin: true,
-          
-          // 👇 TRADUÇÃO COM O DELAY ADICIONADO PARA O TYPESCRIPT NÃO RECLAMAR 👇
-          promptOptions: {
-            slidedown: {
-              prompts: [
-                {
-                  type: "push",
-                  autoPrompt: false,
-                  text: {
-                    actionMessage: "Queremos te enviar alertas sobre o seu jejum e a sua evolução na alcateia.",
-                    acceptButton: "Permitir",
-                    cancelButton: "Agora Não"
-                  },
-                  delay: {
-                    pageViews: 1,
-                    timeDelay: 0
-                  }
-                }
-              ]
-            }
-          }
-
+          // A MÁGICA: Janela branca removida. O OneSignal agora age em silêncio.
         });
         console.log('🐺 Radar OneSignal ativado na matilha!');
       } catch (error) {
